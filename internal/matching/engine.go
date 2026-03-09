b8c33399fdbbdd7be601117059030c6b28d80448
chaeho5 <dkscogh1@gmail.com>
2026-03-09 21:50:55 +0900

    feat: 매칭엔진 로직 구현

--- internal/matching/engine.go
+++ internal/matching/engine.go
@@ -13,6 +13,7 @@
 package matching
 
 import (
+	"time"
 	"github.com/Go-Exchange-Project/Go-exchange-back/internal/model"
 )
 
@@ -22,10 +23,110 @@ type MatchingEngine struct {
 	TradeCh chan *model.Trade // 체결 결과 내보낼 채널
 }
 
-
+// 초기화 함수
 func NewMatchingEngine() *MatchingEngine {
 	return &MatchingEngine{
 		OrderBook: NewOrderBook(),
 		OrderCh: make(chan *Order, 1024),
+		TradeCh: make(chan *model.Trade, 1024),
+	}
+}
+
+/*
+1. 고루틴 실행
+2. 채널에서 주문을 계속 기다리고 -> order가 들어올 때마다 실행
+3. 주문 들어오면 오더북에 추가하고
+4. 매칭 시도 하는 함수
+*/
+func (me *MatchingEngine) Start() {
+	// 1
+	go func() {
+		for order := range me.OrderCh {
+			// 2
+			me.OrderBook.AddOrder(order)
+			me.Match(order)
+		}
+	}()
+}
+
+/*
+1. 매수 주문이면 -> 매도 오더북에서 가장 낮은 가격 찾기
+2. 매도 주문이면 -> 매수 오더북에서 가장 높은 가격 찾기
+3. 가격 조건 맞으면 -> 체결 (Trade 생성)
+4. 조건 안 맞으면 -> 오더북에서 대기(이미 AddOrder로 추가됨)
+*/
+
+func (me *MatchingEngine) Match(order *Order) {
+	if order.Side == model.OrderSideBuy {
+		sellLevel, ok := me.OrderBook.SellOrders.Min()
+		if ok {
+			if order.Price.GreaterThanOrEqual(sellLevel.Price) {
+				// 1. 매도 주문 꺼내기
+				sellOrder := sellLevel.Orders.PopFront()
+				// 2. 체결 수량 계산(매수/매도 중 더 작은 쪽)
+				tradeQty := order.Amount.Min(sellOrder.Amount)
+				// 3. 트레이드 생성
+				trade := &model.Trade{
+					CoinSymbol:  order.CoinSymbol,
+    				Price:       sellLevel.Price,
+    				Quantity:    tradeQty,
+    				TradedAt:    time.Now(),
+    				BuyOrderID:  order.ID,
+    				SellOrderID: sellOrder.ID,
+				}
+				// 4.TradeCh 채널로 체결 결과 내보내기
+				me.TradeCh <- trade
+
+				// 5. 남은 수량 처리
+				order.Amount = order.Amount.Sub(tradeQty)
+				order.FilledAmount = order.FilledAmount.Add(tradeQty)
+				sellOrder.Amount = sellOrder.Amount.Sub(tradeQty)
+
+				if sellOrder.Amount.IsZero() {
+    				if sellLevel.Orders.Len() == 0 {
+        				me.OrderBook.SellOrders.Delete(sellLevel)
+    				}
+				} else {
+    				sellLevel.Orders.PushFront(sellOrder)
+				}
+
+				if order.Amount.IsZero() {
+    				me.OrderBook.RemoveOrder(order)
+				}
+			}
+		}
+	} else if order.Side == model.OrderSideSell{
+		buyLevel, ok := me.OrderBook.BuyOrders.Max()
+		if ok {
+        	if buyLevel.Price.GreaterThanOrEqual(order.Price) {
+            	buyOrder := buyLevel.Orders.PopFront()
+            	tradeQty := order.Amount.Min(buyOrder.Amount)
+            	trade := &model.Trade{
+                	CoinSymbol:  order.CoinSymbol,
+                	Price:       buyLevel.Price,
+                	Quantity:    tradeQty,
+                	TradedAt:    time.Now(),
+                	BuyOrderID:  buyOrder.ID,
+                	SellOrderID: order.ID,
+            	}
+            	me.TradeCh <- trade
+
+				order.Amount = order.Amount.Sub(tradeQty)
+				order.FilledAmount = order.FilledAmount.Add(tradeQty)
+				buyOrder.Amount = buyOrder.Amount.Sub(tradeQty)
+
+				if buyOrder.Amount.IsZero() {
+    				if buyLevel.Orders.Len() == 0 {
+        				me.OrderBook.BuyOrders.Delete(buyLevel)
+    				}
+				} else {
+    				buyLevel.Orders.PushFront(buyOrder)
+				}
+				
+				if order.Amount.IsZero() {
+    				me.OrderBook.RemoveOrder(order)
+				}
+        	}
+    	}
 	}
 }
