9abb4a8b132f142b00981e8e110b10de7b127846
chaeho5 <dkscogh1@gmail.com>
2026-03-10 22:44:53 +0900

    test: testify라이브러리 설치 및 테스트 파일 생성

---
+++ internal/matching/engine_test.go
@@ -0,0 +1,15 @@
+package matching
+
+import (
+	"testing"
+)
+
+func TestMatch_매수가가_매도가보다_높으면_체결된다(t *testing.T) {
+	me := NewMatchingEngine()
+	me.Start()
+	me.OrderCh -> &Order{
+		sellOrder := &Order{
+			
+		}
+	}
+}
