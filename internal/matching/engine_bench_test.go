0e5e4f67deedb5e7189d37a7029f0816703ed557
chaeho5 <dkscogh1@gmail.com>
2026-07-05 22:02:48 +0900

    test: add orderbook-depth scaling benchmark

--- internal/matching/engine_bench_test.go
+++ internal/matching/engine_bench_test.go
@@ -1,6 +1,7 @@
 package matching
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/Go-Exchange-Project/Go-exchange-back/internal/model"
@@ -29,5 +30,29 @@ func BenchmarkMatch_ImmediateCross(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		me.Match(testOrder(uint(i+2), "BTC", model.OrderSideBuy, 50000, 1))
+	}
+}
+
+func BenchmarkOrderBookDepth(b *testing.B) {
+	depths := []int{100, 1000, 10000}
+	for _, depth := range depths {
+		b.Run(fmt.Sprintf("depth=%d", depth), func(b *testing.B) {
+			me := NewMatchingEngine()
+			for i := 0; i < depth; i++ {
+				me.Match(testOrder(uint(i+1), "BTC", model.OrderSideSell, int64(50000+i), 1))
+			}
+
+			done := make(chan struct{})
+			go drainEngineEvents(me, done)
+			defer close(done)
+
+			b.ReportAllocs()
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				orderID := uint(100000 + i*2)
+				me.Match(testOrder(orderID, "BTC", model.OrderSideBuy, 50000, 1))
+				me.Match(testOrder(orderID+1, "BTC", model.OrderSideSell, 50000, 1))
+			}
+		})
 	}
 }
